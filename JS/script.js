843c8d4d0c0493e7ec8e9f46a5aa1313d32e2010
Madhubalatn9 <madhubalatn9@gmail.com>
2026-07-31 11:07:00 +0530

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function hideFunction(){
+    const dropdownRecents = document.getElementById("dropdownRecents");
+    if(dropdownRecents.style.display == "none"){
+        dropdownRecents.style.display = "block";
+    }
+    else{
+        dropdownRecents.style.display = "none";
+    }
+
+
+}
